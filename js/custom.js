f9ac2af84d2fbf9ff1590d5cdbbb5f15df1ca6df
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-09-25 21:32:43 -0400

    Update Quienes Somos

--- js/custom.js
+++ js/custom.js
@@ -12,17 +12,17 @@ $(".client_owl-carousel").owlCarousel({
     dots: false,
     nav: true,
     navText: [],
-    autoplay: 100,
+    autoplay: 10,
     autoplayHoverPause: true,   
     responsive: {
         0: {
             items: 1
         },
         768: {
-            items: 4
+            items: 3
         },
         1000: {
-            items: 4
+            items: 3
         }
     }
 });
